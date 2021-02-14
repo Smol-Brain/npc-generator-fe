d979b278fbab7a58ae9c31edef58bf2bbacce198
Corey Lantz <clantz@homex.com>
2021-02-14 00:43:35 -0500

    Update CharacterCard styles

--- src/components/specific/CharacterCard/CharacterCard.tsx
+++ src/components/specific/CharacterCard/CharacterCard.tsx
@@ -1,8 +1,11 @@
-import { Card } from 'components/common/Card'
+import { useEffect, useState } from 'react'
+import { CSSTransition } from 'react-transition-group'
+
 import { Stack } from 'components/layouts/Stack'
-import { SCALE } from 'styles/variables'
+import { ANIMATION_TIME, SCALE } from 'styles/variables'
 import { ICharacter } from 'types'
 
+import { CharacterContainer } from './styled'
 interface ICharacterCardProps
     extends ICharacter,
         React.HTMLAttributes<HTMLDivElement> {}
@@ -10,33 +13,58 @@ interface ICharacterCardProps
 const CARD_SCALE = SCALE['s0']
 
 export const CharacterCard = ({
-    race,
-    gender,
-    pronouns,
     firstName,
-    lastName,
+    gender,
+    height,
+    id,
     job,
-    wealth,
+    lastName,
     lifeStage,
+    pronouns,
+    race,
+    wealth,
+    ...rest
 }: ICharacterCardProps) => {
+    const [isMounted, setIsMounted] = useState(false)
+    // Ignore flicker on transition
+    const [isShown, setIsShown] = useState(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
+
     return (
-        <Card>
-            <Stack space={CARD_SCALE}>
-                <h2>
-                    {firstName} {lastName}
-                </h2>
-                <p>{`${gender} - ${race} - ${pronouns}`}</p>
-                <ul>
-                    <li>LifeStage: {lifeStage}</li>
-                </ul>
-            </Stack>
-            <Stack space={CARD_SCALE}>
-                <h3>Life style</h3>
-                <ul>
-                    <li>Job: {job}</li>
-                    <li>Wealth: {wealth}</li>
-                </ul>
-            </Stack>
-        </Card>
+        <CSSTransition
+            classNames="fade-in"
+            in={isMounted}
+            timeout={ANIMATION_TIME}
+            onEntered={() => {
+                setIsShown(true)
+            }}
+        >
+            <CharacterContainer
+                className={isShown ? 'shown' : ''}
+                borderWidth="0"
+                {...rest}
+            >
+                <Stack space={CARD_SCALE}>
+                    <h2 id={id}>
+                        {firstName} {lastName}
+                    </h2>
+                    <p>{`${gender} - ${race} - ${pronouns}`}</p>
+                    <ul>
+                        <li>Age: {lifeStage}</li>
+                        <li>Height: {height}</li>
+                    </ul>
+                </Stack>
+                <Stack space={CARD_SCALE}>
+                    <h3>Life style</h3>
+                    <ul>
+                        <li>Job: {job}</li>
+                        <li>Wealth: {wealth}</li>
+                    </ul>
+                </Stack>
+            </CharacterContainer>
+        </CSSTransition>
     )
 }
---
+++ src/components/specific/CharacterCard/styled.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+import { Card } from 'components/common/Card'
+import { ANIMATION_TIME_MS, COLORS, SCALE } from 'styles/variables'
+
+export const CharacterContainer = styled(Card)`
+    position: relative;
+    box-shadow: 0 3px 1px rgba(0, 0, 0, 0.05);
+    opacity: 0;
+
+    &.shown {
+        opacity: 1;
+    }
+
+    &.fade-in-enter {
+        opacity: 0;
+        transform: translateY(${SCALE['s-1']});
+    }
+
+    &.fade-in-enter-active {
+        opacity: 1;
+        transform: translateY(0);
+        transition: opacity ${ANIMATION_TIME_MS}, transform ${ANIMATION_TIME_MS};
+    }
+
+    &.fade-in-exit {
+        opacity: 1;
+    }
+    &.fade-in-exit-active {
+        opacity: 0;
+        transform: scale(0.9);
+        transition: opacity 300ms, transform 300ms;
+    }
+
+    &:before,
+    &:after {
+        content: '';
+        height: 0.17em;
+        position: absolute;
+        left: 0;
+        right: 0;
+        -webkit-clip-path: polygon(
+            0% 0%,
+            5% 100%,
+            10% 0%,
+            15% 100%,
+            20% 0%,
+            25% 100%,
+            30% 0%,
+            35% 100%,
+            40% 0%,
+            45% 100%,
+            50% 0%,
+            55% 100%,
+            60% 0%,
+            65% 100%,
+            70% 0%,
+            75% 100%,
+            80% 0%,
+            85% 100%,
+            90% 0%,
+            95% 100%,
+            100% 0%
+        );
+    }
+
+    &:before {
+        background-color: ${COLORS.lightish};
+        top: -0.07em;
+    }
+    &:after {
+        background-color: ${COLORS.light};
+        bottom: -0.14em;
+    }
+`
