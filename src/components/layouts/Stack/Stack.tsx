284d54e9565889319c0b1f86e4f693628d7453fd
Corey Lantz <clantz@homex.com>
2021-02-17 22:50:19 -0500

    Update Stack type

--- src/components/layouts/Stack/Stack.tsx
+++ src/components/layouts/Stack/Stack.tsx
@@ -1,14 +1,18 @@
+import styled from 'styled-components'
 import { SCALE } from 'styles/variables'
 
 import { StackContainer } from './styled'
 
+const Thing = styled.a``
 export interface IStackProps extends React.HTMLAttributes<HTMLDivElement> {
+    as?: string | React.ComponentType<any>
     recursive?: boolean
     space?: string
     splitAfter?: number
 }
 
 export const Stack = ({
+    as,
     children,
     recursive = false,
     space = SCALE.s1,
@@ -17,6 +21,7 @@ export const Stack = ({
 }: IStackProps) => {
     return (
         <StackContainer
+            as={as}
             recursive={recursive}
             space={space}
             splitAfter={splitAfter}
