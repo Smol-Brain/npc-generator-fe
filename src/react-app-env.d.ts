191300b57a939f2e085f4f646aad2a32fed5f789
Corey Lantz <clantz@homex.com>
2021-02-22 12:49:08 -0500

    Add env types

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,15 @@
 /// <reference types="react-scripts" />
+declare namespace NodeJS {
+    interface ProcessEnv {
+        NODE_ENV: 'development' | 'production' | 'test'
+        REACT_APP_GOOGLE_API_KEY: string
+        REACT_APP_GOOGLE_AUTH_DOMAIN: string
+        REACT_APP_GOOGLE_PROJECT_ID: string
+        REACT_APP_GOOGLE_STORAGE_BUCKET: string
+        REACT_APP_GOOGLE_MESSAGING_ID: string
+        REACT_APP_GOOGLE_APP_ID: string
+
+        REACT_APP_NPC_API: string
+        REACT_APP_NPC_API_GENERATE: string
+    }
+}
